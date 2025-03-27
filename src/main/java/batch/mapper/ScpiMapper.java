d299ae1d2bbaeba4a17acab7a98b153d48930ad1
Ouali Abdiche <oualiabdiche@outlook.fr>
2025-03-28 00:27:25 +0100

    fix bugs

--- src/main/java/batch/mapper/ScpiMapper.java
+++ src/main/java/batch/mapper/ScpiMapper.java
@@ -1,13 +1,18 @@
 package batch.mapper;
 
+import batch.dto.LocationDtoOut;
 import batch.dto.ScpiDtoOut;
+import batch.dto.SectorDtoOut;
 import batch.dto.StatYearDtoOut;
+import batch.entity.Location;
 import batch.entity.Scpi;
+import batch.entity.Sector;
 import batch.entity.StatYear;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -29,6 +34,35 @@ public interface ScpiMapper {
                 .reconstitutionValue(statYears.get(0).getReconstitutionValue())
                 .sharePrice(statYears.get(0).getSharePrice())
                 .build()
+                : null;
+    }
+
+    @Named("highestPercentageLocation")
+    default LocationDtoOut getHighestPercentageLocation(List<Location> locations) {
+        return (locations != null && !locations.isEmpty())
+                ? locations.stream()
+                .filter(loc -> loc.getCountryPercentage() != null) // Filtrer les valeurs nulles
+                .max(Comparator.comparing(Location::getCountryPercentage)) // Comparaison directe
+                .map(item -> LocationDtoOut
+                        .builder()
+                        .id(item.getId())
+                        .countryPercentage(item.getCountryPercentage()) // Pas besoin de conversion
+                        .build())
+                .orElse(null)
+                : null;
+    }
+
+    @Named("highestPercentageSector")
+    default SectorDtoOut getHighestPercentageSector(List<Sector> sectors) {
+        return (sectors != null && !sectors.isEmpty())
+                ? sectors.stream()
+                .max(Comparator.comparing(Sector::getSectorPercentage))
+                .map(sector -> SectorDtoOut
+                        .builder()
+                        .id(sector.getId())
+                        .sectorPercentage(sector.getSectorPercentage())
+                        .build())
+                .orElse(null)
                 : null;
     }
 }
